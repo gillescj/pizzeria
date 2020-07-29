28730bb092cb432b5d6d3a01433fa8a2628e2f67
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-29 17:09:46 -0400

    Add styling global variables

---
+++ src/styling.js
@@ -0,0 +1,12 @@
+export const COLOURS = {
+    coral: '#ff7f50',
+    orange: '#ff4400',
+};
+
+export const QUERIES = {
+    small: 'max-width: 600px',
+    medium: 'max-width: 850px',
+    large: 'max-width: 1100px',
+    xLarge: 'max-width: 1400px',
+    maxWidth: '1400px',
+};
