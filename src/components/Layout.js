032ec02ad01aa65cb90eed3aac81669ad0dce1f3
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-30 13:50:23 -0400

    Add general Header - Main - Footer style to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
 
+const Container = styled.div`
+    display: grid;
+    align-items: start;
+    min-height: 100vh;
+    grid-template-rows: auto 1fr auto;
+`;
+
 const Layout = ({ children }) => {
     return (
-        <>
+        <Container>
             <Header />
             <main>{children}</main>
             <Footer />
-        </>
+        </Container>
     );
 };
 
