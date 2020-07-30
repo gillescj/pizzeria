9ecb6113738bf3c87ea75e56ec6b9ed689be3c7d
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-30 15:26:24 -0400

    Move Footer.js into Folder folder Split styling into seperate file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,41 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import { QUERIES, COLOURS } from 'src/styling';
-
-const Container = styled.div`
-    display: grid;
-    grid-template-columns: minmax(1rem, ${QUERIES.maxWidth});
-    justify-content: center;
-    background: ${COLOURS.veryDarkBlue};
-`;
-
-const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    padding: 2rem;
-    grid-gap: 5rem 2rem;
-
-    @media (${QUERIES.large}) {
-        grid-template-columns: repeat(2, 1fr);
-    }
-    @media (${QUERIES.small}) {
-        grid-template-columns: repeat(1, 1fr);
-    }
-`;
-
-const InfoContainer = styled.section`
-    color: white;
-    h3 {
-        padding-bottom: 2.5rem;
-        text-transform: uppercase;
-        font-weight: normal;
-    }
-
-    li {
-        padding: 0.25rem 0;
-        font-weight: 300;
-    }
-`;
+import { Container, Wrapper, InfoContainer } from './styles';
 
 const Footer = () => {
     return (
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { QUERIES, COLOURS } from 'src/styling';
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: minmax(1rem, ${QUERIES.maxWidth});
+    justify-content: center;
+    background: ${COLOURS.veryDarkBlue};
+`;
+
+export const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    padding: 2rem;
+    grid-gap: 5rem 2rem;
+
+    @media (${QUERIES.large}) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media (${QUERIES.small}) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`;
+
+export const InfoContainer = styled.section`
+    color: white;
+    h3 {
+        padding-bottom: 2.5rem;
+        text-transform: uppercase;
+        font-weight: normal;
+    }
+
+    li {
+        padding: 0.25rem 0;
+        font-weight: 300;
+    }
+`;
