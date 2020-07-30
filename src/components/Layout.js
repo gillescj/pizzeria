99611c7a086f594837f95f042ecf0b7544468544
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-30 15:26:57 -0400

    Add Wrapper for main content Padding, maxwidth, justify center

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { QUERIES } from 'src/styling';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -10,11 +11,22 @@ const Container = styled.div`
     grid-template-rows: auto 1fr auto;
 `;
 
+const MainWrapper = styled.div`
+    display: grid;
+    grid-template-columns: minmax(1rem, ${QUERIES.maxWidth});
+    justify-content: center;
+    main {
+        padding: 1rem;
+    }
+`;
+
 const Layout = ({ children }) => {
     return (
         <Container>
             <Header />
-            <main>{children}</main>
+            <MainWrapper>
+                <main>{children}</main>
+            </MainWrapper>
             <Footer />
         </Container>
     );
