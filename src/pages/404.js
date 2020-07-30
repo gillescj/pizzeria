bd04aac49072f34f36169824fcfff833b2c6f19a
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-30 16:17:18 -0400

    Add content / styles to 404 page Add forwn svg

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import { COLOURS } from 'src/styling';
+import FrownSVG from 'src/assets/svgs/frown.svg';
+import Layout from 'src/components/Layout';
+import { Link } from 'gatsby';
+
+const Container = styled.div`
+    display: grid;
+    justify-items: center;
+    grid-gap: 2rem;
+    h1 {
+        color: ${COLOURS.darkGrey};
+    }
+    p {
+        max-width: 30rem;
+        font-weight: 400;
+    }
+    a {
+        color: ${COLOURS.darkBlue};
+    }
+    margin: 3rem 0;
+`;
 
 const error = () => {
-    return <h1>This is our error page</h1>;
+    return (
+        <Layout>
+            <Container>
+                <h1>Oops, something went wrong </h1>
+                <FrownSVG />
+                <p>
+                    The page you tried page couldn't be found. You may have typed in the
+                    address incorrectly or used an outdated link. Navigate to a different
+                    page or <Link to="/">click here</Link> to go back to the main page.
+                </p>
+            </Container>
+        </Layout>
+    );
 };
 
 export default error;
