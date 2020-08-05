ebeade777179cd55f0faeaf2187b8af25a69492c
Colin Gillespie <ratchetandcolin@gmail.com>
2020-08-05 02:56:38 -0400

    Add PageHeader to all pages

--- src/pages/delivery.js
+++ src/pages/delivery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Layout from 'src/components/Layout';
+import PageHeader from 'src/components/PageHeader';
 
 const Container = styled.div`
     display: grid;
@@ -8,7 +9,8 @@ const Container = styled.div`
 
 const Delivery = () => {
     return (
-        <Layout>
+        <Layout padding="0" maxWidth="100%">
+            <PageHeader pageName="Delivery" />
             <Container>Hello from Delivery</Container>
         </Layout>
     );
