85692d1a6207d43274327130cc0c763859a2d8d5
Colin Gillespie <ratchetandcolin@gmail.com>
2020-08-05 21:09:21 -0400

    Create Map component shell

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background: hsl(190, 60%, 60%);
+`;
+
+const Map = () => {
+    return <Container>Map</Container>;
+};
+
+export default Map;
