63306d648c2e268fc34bf38306fd98547d18cc3b
Colin Gillespie <ratchetandcolin@gmail.com>
2020-08-17 21:25:02 -0400

    Add MenuSection component

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,16 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import Layout from 'src/components/Layout';
 import PageHeader from 'src/components/PageHeader';
+import MenuSection from '../components/MenuSection';
 
 const Container = styled.div`
     display: grid;
+    grid-template-columns: minmax(1rem, 60rem);
+    justify-content: center;
 `;
 
 const Menu = () => {
     return (
         <Layout padding="0" maxWidth="100%">
             <PageHeader pageName="Menu" />
-            <Container>Hello from Menu</Container>
+            <Container>
+                <MenuSection />
+            </Container>
         </Layout>
     );
 };
