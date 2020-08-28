0f1c97ba32af86ebb29baf680505e1da0c7e9315
Colin Gillespie <ratchetandcolin@gmail.com>
2020-08-27 20:25:37 -0400

    Add section to menu Starters

--- src/utils/menuList.js
+++ src/utils/menuList.js
@@ -1,5 +1,35 @@
 const menuList = [
     {
+        name: 'starters',
+        info: { heading: 'Starters', subHeading: 'Appetizers and Shareables' },
+        sectionItems: [
+            {
+                title: 'Pepperoni Pizza',
+                price: '$12.50',
+                description: `Bushwick fam chia pug, man braid PBR&B mlkshk vaporware enamel pin mumblecore occupy. Succulents iceland stumptown, XOXO fixie tilde biodiesel DIY palo santo vape shoreditch venmo drinking vinegar.`,
+                vegetarian: true,
+            },
+            {
+                title: 'Pepperoni Pizza',
+                price: '$12.50',
+                description: `Bushwick fam chia pug, man braid PBR&B mlkshk vaporware enamel pin mumblecore occupy. Succulents iceland stumptown, XOXO fixie tilde biodiesel DIY palo santo vape shoreditch venmo drinking vinegar.`,
+                vegetarian: true,
+            },
+            {
+                title: 'Pepperoni Pizza',
+                price: '$12.50',
+                description: `Bushwick fam chia pug, man braid PBR&B mlkshk vaporware enamel pin mumblecore occupy. Succulents iceland stumptown, XOXO fixie tilde biodiesel DIY palo santo vape shoreditch venmo drinking vinegar.`,
+                vegetarian: true,
+            },
+            {
+                title: 'Pepperoni Pizza',
+                price: '$12.50',
+                description: `Bushwick fam chia pug, man braid PBR&B mlkshk vaporware enamel pin mumblecore occupy. Succulents iceland stumptown, XOXO fixie tilde biodiesel DIY palo santo vape shoreditch venmo drinking vinegar.`,
+                vegetarian: true,
+            },
+        ],
+    },
+    {
         name: 'pizza',
         info: { heading: 'Slice City', subHeading: 'Gourmet Pizzas' },
         sectionItems: [
